94bb1a104d663f18913249408fd9314aa94da461
cat <cat@scaleapi.com>
2019-05-17 21:31:14 -0700

    .

--- bleed-blue/src/App.js
+++ bleed-blue/src/App.js
@@ -3,8 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import Game from './game';
 
-const eventLog = [];
-
 const gameState = {
   day: 3,
   players: [1, 2],
@@ -12,13 +10,13 @@ const gameState = {
   player: 1,
   role: 'Hitler',
 }
-// function App() {
-//   return <Game id='adsfae' gameState={gameState} doAction={doAction}></Game>;
-// }
+
 class App extends React.Component {
   constructor(props){
     super(props);
-    this.state = {count: 0};
+    this.state = {
+      count: 0
+    };
   }
   render() {
     return (
